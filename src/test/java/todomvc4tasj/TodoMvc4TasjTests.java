dceb152e19c72c6bfda788c11ce2343a2c4cebaf
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-09-07 14:34:28 +0300

    tests united

--- src/test/java/todomvc4tasj/TodoMvc4TasjTests.java
+++ src/test/java/todomvc4tasj/TodoMvc4TasjTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static com.codeborne.selenide.Condition.enabled;
@@ -11,23 +12,26 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TodoMvc4TasjTests {
 
-    {
-        Configuration.timeout = 4000;
+    @Test
+    public void allTests() {
+        Configuration.timeout = 6000;
 
         open("http://todomvc4tasj.herokuapp.com/");
-        Selenide.Wait().until(ExpectedConditions.jsReturnsValue(
-                "return Object.keys(require.s.contexts._.defined).length === 39"));
         Selenide.Wait().until(ExpectedConditions.jsReturnsValue(
                 "return Object.keys(require.s.contexts._.defined).length === 39"));
 
         $("#new-todo").shouldBe(enabled).setValue("a").pressEnter();
         $("#new-todo").shouldBe(enabled).setValue("b").pressEnter();
         $("#new-todo").shouldBe(enabled).setValue("c").pressEnter();
-    }
 
-    @Test
-    public void todoShouldCreated() {
+        // todoShouldCreated
         $$("#todo-list>li").shouldHave(CollectionCondition.exactTexts("a", "b", "c"));
-    }
 
+        // todoShouldEdited
+        $("#todo-list>li:nth-of-type(2)").doubleClick();
+        Selenide.executeJavaScript("$(\"#todo-list>li:nth-of-type(2) .edit\").select()");
+        $("#todo-list>li:nth-of-type(2) .edit").sendKeys("D" + Keys.ENTER);
+
+        $$("#todo-list>li").shouldHave(CollectionCondition.exactTexts("a", "D", "c"));
+    }
 }
